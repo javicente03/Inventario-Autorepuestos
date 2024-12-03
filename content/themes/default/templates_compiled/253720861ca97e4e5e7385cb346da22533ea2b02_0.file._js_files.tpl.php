ae9356289ec6cd25bd296065df51a891529a9e30
javier <javicentego@gmail.com>
2024-12-03 14:41:22 -0400

    fix templates

---
+++ content/themes/default/templates_compiled/253720861ca97e4e5e7385cb346da22533ea2b02_0.file._js_files.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 3.1.40, created on 2024-11-30 18:39:14
+  from 'C:\xampp\htdocs\casper\Inventario-Autorepuestos\content\themes\default\templates\_js_files.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.40',
+  'unifunc' => 'content_674b5bd2f0b985_89495807',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '253720861ca97e4e5e7385cb346da22533ea2b02' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\casper\\Inventario-Autorepuestos\\content\\themes\\default\\templates\\_js_files.tpl',
+      1 => 1709126301,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_674b5bd2f0b985_89495807 (Smarty_Internal_Template $_smarty_tpl) {
+?><!-- Initialize --><?php echo '<script'; ?>
+>/* initialize vars */var site_path = "<?php echo $_smarty_tpl->tpl_vars['base_url']->value;?>
+";<?php echo '</script'; ?>
+><?php }
+}
